57c76bd2e7907c15216f1d3981ef0e1b2a325380
Dmitry <zatulenko@gmail.com>
2018-06-17 12:27:52 +0300

    Отключен подробный вывод ошибок

--- configuration.php
+++ configuration.php
@@ -16,7 +16,7 @@ class JConfig {
 	public $db = 'prodogsinfo';
 	public $dbprefix = 'gy3te_';
 	public $dbtype = 'mysqli';
-	public $debug = '1';
+	public $debug = '0';
 	public $debug_lang = '0';
 	public $display_offline_message = '1';
 	public $editor = 'tinymce';
